e1d1df836db0cb8511dbf626d3d27f8d63e76817
Pasinee Rukpromrach <64160056@go.buu.ac.th>
2023-02-17 01:03:41 +0700

    install icon front

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,25 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
+import { aliases, mdi } from "vuetify/iconsets/mdi";
 
 // Vuetify
 import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
+import "@mdi/font/css/materialdesignicons.css";
 
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: "mdi",
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
 });
 
 const app = createApp(App);
