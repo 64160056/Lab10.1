3bd7004a00bc2bc67355f3d3f3cfa1a670ceb923
Pasinee Rukpromrach <64160056@go.buu.ac.th>
2023-02-17 01:17:01 +0700

    layout

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,15 @@ import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <v-icon icon="mdi-home" />
+  <v-app>
+    <v-app-bar> </v-app-bar>
+    <v-navigation-drawer>
+      <v-list>
+        <v-list-item title="Navigation drawer"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main></v-main>
+  </v-app>
 </template>
 
 <style scoped></style>
