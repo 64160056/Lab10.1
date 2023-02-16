26b43e2a87c74f43c21fee3c2ecda358b37de299
Pasinee Rukpromrach <64160056@go.buu.ac.th>
2023-02-17 02:53:12 +0700

    RouterView

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -7,46 +7,14 @@ const rail = ref(true);
 
 <template>
   <v-layout>
-    <v-app-bar> </v-app-bar>
+    <v-app-bar> <RouterView name="headder"> </RouterView> </v-app-bar>
     <v-navigation-drawer
       v-model="drawer"
       :rail="rail"
       permanent
       @click="rail = false"
     >
-      <v-list-item
-        prepend-avatar="https://randomuser.me/api/portraits/men/85.jpg"
-        title="John Leider"
-        nav
-      >
-        <template v-slot:append>
-          <v-btn
-            variant="text"
-            icon="mdi-chevron-left"
-            @click.stop="rail = !rail"
-          ></v-btn>
-        </template>
-      </v-list-item>
-
-      <v-divider></v-divider>
-
-      <v-list density="compact" nav>
-        <v-list-item
-          prepend-icon="mdi-home-city"
-          title="Home"
-          value="home"
-        ></v-list-item>
-        <v-list-item
-          prepend-icon="mdi-account"
-          title="My Account"
-          value="account"
-        ></v-list-item>
-        <v-list-item
-          prepend-icon="mdi-account-group-outline"
-          title="Users"
-          value="users"
-        ></v-list-item>
-      </v-list>
+      <RouterView name="menu"></RouterView>
     </v-navigation-drawer>
     <v-main></v-main>
     <v-main><RouterView></RouterView></v-main>
