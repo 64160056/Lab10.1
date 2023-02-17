e441e1c014c34823e0ebe658b70297a08367eb15
Pasinee Rukpromrach <64160056@go.buu.ac.th>
2023-02-17 22:36:40 +0700

    setup Mianlayout

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import MainLayout from "@/layouts/MainLayout.vue";
+import MainMenu from "./components/menus/MainMenu.vue";
 </script>
 
 <template>
   <MainLayout></MainLayout>
+  <MainMenu></MainMenu>
 </template>
 
 <style scoped></style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,22 +1,10 @@
-<script setup lang="ts">
-import { ref } from "vue";
-
-const drawer = ref(true);
-const rail = ref(true);
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <v-layout>
     <v-app-bar> <RouterView name="headder"> </RouterView> </v-app-bar>
-    <v-navigation-drawer
-      v-model="drawer"
-      :rail="rail"
-      permanent
-      @click="rail = false"
-    >
-      <RouterView name="menu"></RouterView>
-    </v-navigation-drawer>
-    <v-main></v-main>
+    <RouterView name="menu"></RouterView>
+
     <v-main><RouterView></RouterView></v-main>
   </v-layout>
 </template>
