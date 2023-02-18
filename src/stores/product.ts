cb74822dc655fdffe8c58b74d7974544f328c7bd
Pasinee Rukpromrach <64160056@go.buu.ac.th>
2023-02-18 23:34:16 +0700

    axio get Product

--- src/stores/product.ts
+++ src/stores/product.ts
@@ -1,8 +1,18 @@
 import { ref, computed } from "vue";
 import { defineStore } from "pinia";
-import type { Product } from "@/types/Product";
+import type Product from "@/types/Product";
+import axios from "axios";
 
 export const useProductStore = defineStore("product", () => {
   const products = ref<Product[]>([]);
-  return { products };
+  async function getProducts() {
+    try {
+      const res = await axios.get("http://localhost:3000/products");
+      products.value = res.data;
+      console.log(res);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  return { products, getProducts };
 });
--- src/views/products/ProductView.vue
+++ src/views/products/ProductView.vue
@@ -1,7 +1,11 @@
 <script lang="ts" setup>
 import { useProductStore } from "@/stores/product";
+import { onMounted } from "vue";
 
 const productStore = useProductStore();
+onMounted(async () => {
+  await productStore.getProducts();
+});
 </script>
 <template>
   <div>{{ productStore.products }}</div>
